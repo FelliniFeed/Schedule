9a44a297b303891f78a72012b153151bc2b0cbd6
FelliniFeed <99238935+FelliniFeed@users.noreply.github.com>
2024-12-12 20:45:04 +0800

    add migration

---
+++ schema/000001_init.down.sql
@@ -0,0 +1,13 @@
+DROP TABLE class;
+
+DROP TABLE student;
+
+DROP TABLE student_class;
+
+DROP TABLE teacher;
+
+DROP TABLE subject;
+
+DROP TABLE timepair;
+
+DROP TABLE schedule;
---
+++ schema/000001_init.up.sql
@@ -0,0 +1,52 @@
+CREATE TABLE class (
+    id uuid PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE student (
+    id uuid PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    patronymic VARCHAR(255),
+    birth_day DATE NOT NULL,
+    address VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE student_class (
+    id uuid PRIMARY KEY,
+    student_id uuid NOT NULL,
+    class_id uuid NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES student(id),
+    FOREIGN KEY (class_id) REFERENCES class(id)
+);
+
+CREATE TABLE teacher (
+    id uuid PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    patronymic VARCHAR(255)
+);
+
+CREATE TABLE subject (
+    id uuid PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE timepair (
+    id uuid PRIMARY KEY,
+    start_pair VARCHAR(255) NOT NULL,
+    end_pair VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE schedule (
+    id uuid PRIMARY KEY,
+    date DATE,
+    class_id uuid NOT NULL,
+    number_pair INT NOT NULL,
+    teacher_id uuid NOT NULL,
+    subject_id uuid NOT NULL,
+    class_room VARCHAR(255) NOT NULL,
+    FOREIGN KEY (class_id) REFERENCES class(id),
+    FOREIGN KEY (teacher_id) REFERENCES teacher(id),
+    FOREIGN KEY (subject_id) REFERENCES subject(id)
+);
